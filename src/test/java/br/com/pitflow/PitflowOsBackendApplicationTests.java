f5309495c5435fe1c68ae9d141795c77815326c0
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-01-15 00:07:29 -0300

    chore: first commit

---
+++ src/test/java/br/com/pitflow/PitflowOsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitflowOsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
