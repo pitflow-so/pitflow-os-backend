177438e32a8526c730f9ee19d22d0f062e391b5a
Rafael Moreira <rafaelsmoreiras@gmail.com>
2026-04-14 22:33:59 -0300

    added required helm and kubernetes providers

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -4,6 +4,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+  }
   }
 }
 
